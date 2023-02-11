727f8bd4440b9539741b61327425ee9a9b88f138
Satyendra Mourya <66292112+niraj202@users.noreply.github.com>
2023-02-11 23:43:04 +0530

    fixing the fetching problems

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
+import { useState, useEffect } from 'react';
 
-export default function Home({ posts }) {
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const postsData = await getPosts();
+      setPosts(postsData || []);
+    };
+
+    fetchPosts();
+  }, []);
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
@@ -21,10 +32,3 @@ export default function Home({ posts }) {
   );
 }
 
-// Fetch data at build time
-export async function getStaticProps() {
-  const posts = (await getPosts()) || [];
-  return {
-    props: { posts },
-  };
-}
